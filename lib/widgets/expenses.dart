d8a8e2614c03e7dcdce426c58df6a7f73f0a0de3
ermjonertas <ermert.jonas@yahoo.de>
2023-08-28 13:03:26 +0200

    28.08.2023
    Creating Dummy Data, Creating a Custom List Item with the Card & Spacer Widgets

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,44 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title:'Flutter Course', 
+      amount: 19.99, 
+      date: DateTime.now(),
+      category: Category.work),
+    ),
+    Expense(
+      title:'Cinema', 
+      amount: 9.99, 
+      date: DateTime.(),
+      category: Category.leisure),
+    ),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
