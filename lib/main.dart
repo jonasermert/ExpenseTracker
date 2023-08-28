4132fa5a12342a1661859acee0b06fa0e70e1f88
ermjonertas <ermert.jonas@yahoo.de>
2023-08-28 09:09:30 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:expense_tracker/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+    ),
+  );
+}
